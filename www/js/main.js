aa901588da2ac003e23d1ffd7a4f796de3f6e05c
Seth Whitaker <sdw3489@gmail.com>
2016-09-07 21:13:36 -0400

    Nav Overlay Started

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,21 @@
+(function(){
 
+	$(document).ready(function(){
+		console.log('hi');
+
+		$('.nav-work').on('click', function(e){
+			e.preventDefault();
+			$('#fullpage').fullpage(['sections']);
+			console.log('work clicked');
+			$('.navigation-overlay').toggleClass('show');
+		});
+
+		$('.nav-close').on('click', function(e){
+			e.preventDefault();
+			$('.navigation-overlay').toggleClass('show');
+			$.fn.fullpage.destroy('all');
+		});
+	});
+
+
+})();
