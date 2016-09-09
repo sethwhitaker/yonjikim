47c0ed30a79e8de93e37dfb0d68bd1940cf6a7fc
Seth Whitaker <sdw3489@gmail.com>
2016-09-09 15:41:32 -0400

    Basic Nav working

--- www/js/main.js
+++ www/js/main.js
@@ -1,21 +1,67 @@
-(function(){
+(function($) {
+	'use strict';
+
+	var navigation = {
+
+
+		init: function(){
+			this.$nav = $('.navigation');
+			this.$navWork = this.$nav.find('.nav-work');
+			this.$navMulti = $('.js-nav-multi-button');
+			this.$navOverlay = $('.navigation-overlay');
+			this.$navFull = $("#fullpage");
+			this.addEventHandlers();
+		},
+		openNav: function(e){
+			e.preventDefault();
+			this.$nav.toggleClass('active');
+			this.$navMulti.toggleClass('active');
+			this.$navOverlay.toggleClass('show');
+			this.$navWork.off('click', this.openNav);
+			this.$navMulti.off('click', this.scrollToTop);
+			this.$navMulti.on('click',	$.proxy(this.closeNav, this));
+			this.createFullpage();
+		},
+		closeNav: function(e){
+			e.preventDefault();
+			this.$navMulti.off('click', this.closeNav);
+			this.addEventHandlers();
+			this.$nav.toggleClass('active');
+			this.$navMulti.toggleClass('active');
+			this.$navOverlay.toggleClass('show');
+			this.destroyFullpage();
+		},
+		createFullpage: function(){
+			this.$navFull.fullpage(['sections']);
+		},
+		destroyFullpage: function(){
+			$.fn.fullpage.destroy('all');
+		},
+		scrollToTop: function(e){
+			e.preventDefault;
+
+		    var $this = $(this),
+		        $target = $(this.hash);
+
+		    if( $target.length ){
+		      $('html,body').animate({
+		          scrollTop: $target.offset().top
+		        }, 700, 'swing');
+		    }
+
+		    return false;
+		},
+		addEventHandlers: function(){
+			this.$navWork.on('click', $.proxy(this.openNav, this));
+			this.$navMulti.on('click', this.scrollToTop);
+		}
+
+
+	};
 
 	$(document).ready(function(){
-		console.log('hi');
-
-		$('.nav-work').on('click', function(e){
-			e.preventDefault();
-			$('#fullpage').fullpage(['sections']);
-			console.log('work clicked');
-			$('.navigation-overlay').toggleClass('show');
-		});
-
-		$('.nav-close').on('click', function(e){
-			e.preventDefault();
-			$('.navigation-overlay').toggleClass('show');
-			$.fn.fullpage.destroy('all');
-		});
+		navigation.init();
 	});
 
 
-})();
+})(jQuery);
