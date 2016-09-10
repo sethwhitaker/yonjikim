5e7e43eddfaf3f247ea6edeab8f18fa8362c98d5
Seth Whitaker <sdw3489@gmail.com>
2016-09-09 23:41:57 -0400

    Nav Section aminations

--- www/js/main.js
+++ www/js/main.js
@@ -5,6 +5,7 @@
 
 
 		init: function(){
+			this.isOpen = false;
 			this.$nav = $('.navigation');
 			this.$navWork = this.$nav.find('.nav-work');
 			this.$navMulti = $('.js-nav-multi-button');
@@ -14,6 +15,10 @@
 		},
 		openNav: function(e){
 			e.preventDefault();
+			var _this = this;
+			this.$navWork.off('click', this.openNav);
+			this.$navMulti.off('click', this.scrollToTop);
+			this.$navMulti.on('click',	$.proxy(this.closeNav, this));
 			this.$navFull.fullpage({
 				//Navigation
 				navigation: true,
@@ -21,19 +26,42 @@
 
 				//Scrolling
 				css3: true,
-				scrollingSpeed: 1200,
+				scrollingSpeed: 850,
 				easingcss3: 'cubic-bezier(.86,0,.07,1)',
 
 				//Design
 				controlArrows: true,
-				sectionsColor : ['#143658', '#ffc529']
+				sectionsColor : ['#143658', '#ffc529'],
+				afterRender: function(){
+					console.log("after render");
+					_this.$nav.toggleClass('active');
+					_this.$navMulti.toggleClass('active');
+					_this.$navOverlay.toggleClass('show');
+				},
+				afterLoad:function(anchorLink, index){
+					var $section = $(this);
+					var timeout = (index==1 && _this.isOpen==false)? 700: 0;
+
+					setTimeout(function(){
+						$section.find('h1 a').removeClass('fadeOut fadeOutUp fadeOutDown').addClass('fadeInDown');
+						$section.find('.nav-img img').removeClass('fadeOut fadeOutUp fadeOutDown').addClass('fadeIn');
+					}, timeout);
+
+					_this.isOpen = true;
+				},
+				onLeave: function(index, nextIndex, direction){
+	            var $section = $(this);
+
+	            if(direction =='down'){
+	            	$section.find('h1 a').removeClass('fadeInDown').addClass('fadeOutUp');
+						$section.find('.nav-img img').removeClass('fadeIn').addClass('fadeOut');
+	            }
+	            else if(direction == 'up'){
+	            	$section.find('h1 a').removeClass('fadeInDown').addClass('fadeOutDown');
+						$section.find('.nav-img img').removeClass('fadeIn').addClass('fadeOut');
+	            }
+	         }
 		   });
-			this.$nav.toggleClass('active');
-			this.$navMulti.toggleClass('active');
-			this.$navOverlay.toggleClass('show');
-			this.$navWork.off('click', this.openNav);
-			this.$navMulti.off('click', this.scrollToTop);
-			this.$navMulti.on('click',	$.proxy(this.closeNav, this));
 		},
 		closeNav: function(e){
 			e.preventDefault();
@@ -42,9 +70,12 @@
 			this.$nav.toggleClass('active');
 			this.$navMulti.toggleClass('active');
 			this.$navOverlay.toggleClass('show');
+			this.$navOverlay.find('h1 a').removeClass('fadeOut fadeOutUp fadeOutDown fadeIn fadeInUp fadeInDown');
+			this.$navOverlay.find('.nav-img img').removeClass('fadeOut fadeOutUp fadeOutDown fadeIn fadeInUp fadeInDown');
 			setTimeout(function(){
 				$.fn.fullpage.destroy('all');
 			},800);
+			this.isOpen = false;
 		},
 		scrollToTop: function(e){
 			e.preventDefault;
