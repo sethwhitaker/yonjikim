cb1903f973cc903253b3d458192fa6d9164bd3de
Seth Whitaker <sdw3489@gmail.com>
2016-09-15 17:18:58 -0400

    Scroll animation on the homepage

--- www/js/main.js
+++ www/js/main.js
@@ -1,112 +1,6 @@
 (function($) {
 	'use strict';
 
-	var _containerHeight = 1200;
-	var _width, _height, _scrollHeight;
-	var _movingElements = [];
-	var _scrollOffset = 0;
-	var _scrollPercent = 0;
-	var pre = prefix();
-	var _jsPrefix  = pre.lowercase;
-	if(_jsPrefix == 'moz') _jsPrefix = 'Moz'
-	var _cssPrefix = pre.css;
-	var _positions = [
-		{
-		  name: 'y',
-		  start: {
-				percent: 0, x: 0.1 ,y: 0.1
-		  },
-		  end: {
-				percent: 1, x: 0.3, y: .85
-		  }
-		},
-	];
-
-	resize();
-	initMovingElements();
-
-	function initMovingElements() {
-		for (var i = 0; i < _positions.length; i++) {
-			_positions[i].diff = {
-			  percent: _positions[i].end.percent - _positions[i].start.percent,
-			  x: _positions[i].end.x - _positions[i].start.x,
-			  y: _positions[i].end.y - _positions[i].start.y,
-			}
-			_positions[i].target = {};
-			_positions[i].current = {};
-			var el = document.getElementsByClassName('letter '+_positions[i].name)[0];
-			_movingElements.push(el);
-			console.log(_positions);
-		}
-	}
-
-	function resize() {
-		_width = window.innerWidth;
-		_height = window.innerHeight;
-		_scrollHeight = _containerHeight-_height;
-	}
-
-
-	function updateElements() {
-		for (var i = 0; i < _movingElements.length; i++) {
-			var p = _positions[i];
-			if(_scrollPercent <= p.start.percent) {
-				p.target.x = p.start.x*_width;
-				p.target.y = p.start.y*_containerHeight;
-			} else if(_scrollPercent >= p.end.percent) {
-				p.target.x = p.end.x*_width;
-				p.target.y = p.end.y*_containerHeight;
-			} else {
-				p.target.x = p.start.x*_width + (p.diff.x*(_scrollPercent-p.start.percent)/p.diff.percent*_width);
-				p.target.y = p.start.y*_containerHeight + (p.diff.y*(_scrollPercent-p.start.percent)/p.diff.percent*_containerHeight);
-			}
-
-			// lerp
-			if(!p.current.x) {
-				p.current.x = p.target.x;
-				p.current.y = p.target.y;
-			} else {
-				p.current.x = p.current.x + (p.target.x - p.current.x)*0.1;
-				p.current.y = p.current.y + (p.target.y - p.current.y)*0.1;
-			}
-			_movingElements[i].style[_jsPrefix+'Transform'] = 'translate3d('+p.current.x+'px, '+
-			  p.current.y+'px, 0px)';
-		}
-
-	}
-
-
-
-	function loop() {
-		_scrollOffset = window.pageYOffset || window.scrollTop;
-		_scrollPercent = _scrollOffset/_scrollHeight || 0;
-		updateElements();
-		requestAnimationFrame(loop);
-	}
-	loop();
-
-	window.addEventListener('resize', resize);
-
-	/* prefix detection http://davidwalsh.name/vendor-prefix */
-
-	function prefix() {
-		var styles = window.getComputedStyle(document.documentElement, ''),
-			pre = (Array.prototype.slice
-				.call(styles)
-				.join('')
-				.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
-			)[1],
-			dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
-		return {
-			dom: dom,
-			lowercase: pre,
-			css: '-' + pre + '-',
-			js: pre[0].toUpperCase() + pre.substr(1)
-		};
-	}
-
-
-
 	var navigation = {
 
 
@@ -201,11 +95,95 @@
 			this.$navWork.on('click', $.proxy(this.openNav, this));
 			this.$navMulti.on('click', this.scrollToTop);
 		}
-
-
 	};
 
+	var homepage = function(){
+
+		var flightpath = {
+			y : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: 170,	y: 285}
+					]
+			},
+			o : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: -19,	y: 130}
+					]
+			},
+			n : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: -42,	y: 390}
+					]
+			},
+			j : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: -255, y: 220}
+					]
+			},
+			i : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: -468, y: 80}
+					]
+			},
+			k : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: 440,	y: -140}
+					]
+			},
+			i2 : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: 243,	y: -325}
+					]
+			},
+			m : {
+				curviness: 0,
+				autoRotate: false,
+				values: [
+						{x: -114,	y: -360}
+					]
+			}
+		};
+		// init controller
+		var controller = new ScrollMagic.Controller();
+
+		// create tween
+		var tween = new TimelineMax();
+
+		 tween.to($(".y"), 1, {css:{bezier:flightpath.y}, ease:Power1.easeInOut}, 0)
+				.to($(".o"), 1, {css:{bezier:flightpath.o}, ease:Power1.easeInOut}, 0)
+				.to($(".n"), 1, {css:{bezier:flightpath.n}, ease:Power1.easeInOut}, 0)
+				.to($(".j"), 1, {css:{bezier:flightpath.j}, ease:Power1.easeInOut}, 0)
+				.to($(".i"), 1, {css:{bezier:flightpath.i}, ease:Power1.easeInOut}, 0)
+				.to($(".k"), 1, {css:{bezier:flightpath.k}, ease:Power1.easeInOut}, 0)
+				.to($(".i2"), 1, {css:{bezier:flightpath.i2}, ease:Power1.easeInOut}, 0)
+				.to($(".m"), 1, {css:{bezier:flightpath.m}, ease:Power1.easeInOut}, 0);
+
+
+		// build scene
+		var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 1200, triggerHook: 0, offset: -200})
+						.setPin(".letters")
+						.setTween(tween)
+						.addTo(controller);
+	}
+
 	$(document).ready(function(){
+		if($('body').hasClass('home')){
+			homepage();
+		}
 		navigation.init();
 		new WOW().init();
 	});
